dce04bb6ac3e18376d41a1efcd9d555016aa4d95
JaysQubeXon <noam.jacobson.business@gmail.com>
2017-02-25 11:26:13 +0200

    initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
+import auth from './auth_reducer';
+import msg from './message_reducer';
+
+const rootReducer = combineReducers({
+  form,
+  auth,
+  msg
+});
+
+export default rootReducer;
